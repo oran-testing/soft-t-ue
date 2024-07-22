655ba5ff4f82691bc818823988696b8418a084f0
Charles Ueltschey <chaseuelt@gmail.com>
2024-07-22 15:11:15 -0500

    final debug for Issue #11

--- scripts/install-gnb.sh
+++ scripts/install-gnb.sh
@@ -16,21 +16,6 @@ apt-get install -y cmake make gcc g++ pkg-config libfftw3-dev libmbedtls-dev lib
 apt-get install -y libzmq3-dev
 apt-get install -y net-tools libboost-all-dev libconfig++-dev iperf3 git libxcb-cursor0? libgles2-mesa-dev?
 
-# Add Docker's official GPG key:
-apt-get update
-apt-get install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
-
-# Add the repository to Apt sources:
-echo \
-	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-	tee /etc/apt/sources.list.d/docker.list >/dev/null
-apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
 cd /opt
 
 # Clone and install srsRAN
---
+++ scripts/install-open5gs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+	echo "This script must be run as root."
+	exit 1
+fi
+
+PS4='[DEBUG] '
+set -x
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+	"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+	tee /etc/apt/sources.list.d/docker.list >/dev/null
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+cd /opt
+
+# Clone and install srsRAN
+git clone https://github.com/oran-testing/srsRAN_Project.git
+cd srsRAN_Project
+git checkout ue-tester
+cd docker
+docker compose up --build 5gc
+
+set -x
