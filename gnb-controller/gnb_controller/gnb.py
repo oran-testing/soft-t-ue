5298a4d45c2966718367b20e4446e69eebe669f0
higgigamer19 <texashiggi1@outlook.com>
2024-07-25 17:17:31 -0500

    added tailing for gnb logs and organization

--- gnb-controller/gnb_controller/gnb.py
+++ gnb-controller/gnb_controller/gnb.py
@@ -1,56 +1,73 @@
 import time
 import threading
-#import tailer
+import tailer
 import sys
 import itertools
-
 
 from core_interface import CoreNetwork
 from gnb_interface import Gnb
 
-def spinner_loading(handle, verbose=True):
-    spinner = itertools.cycle(['|', '/', '-', '\\'])
-    while not handle.initialized:
-        if verbose:
-            sys.stdout.write(handle.output + '\n\n' + 50 * '=' + '\n')
-        sys.stdout.write(f"{handle.name} " + next(spinner))
-        sys.stdout.flush()
-        sys.stdout.write('\b')
-        time.sleep(0.1)
+class ue_controller:
+    def __init__(self):
+    	self.reset_file('/tmp/gnb.log')
+    	    
+    def reset_file(self, filename):
+    	with open(filename, 'w') as file:
+    	    pass
 
-
-def ue_controller():
+    def start(self):
+    	#connect to ue controller
+    	
+    	#recieve configuration
+    	
+    	#start gnb and core
+    	gnb_thread = threading.Thread(target=self.start_gnb).start()
+    	core_thread = threading.Thread(target=self.start_core).start()
     
-    #recieve configuration
-    #start gnb and core
-    gnb_thread = threading.Thread(target=start_gnb).start()
-    #core_thread = threading.Thread(target=start_core).start()
+    	#sending metrics
+    	self.metrics()
+    	#wait for restart message from ue controller
+    		#stop gnb, core, metrics, etc
+    		#run a variation of main without calling ue_controller
     
-    #thread sending metrics
-    #metrics_thread = threading.Thread(target=metrics).start()
-    #wait for restart message from ue controller
-    	#stop gnb, core, metrics, etc
-    	#run a variation of main without calling ue_controller
-    
-def metrics():
-    #get metrics
+    def metrics(self):
+    	#get metrics
     	#KPIs
     	#crashes
     	#PCAP between gNB and core
     	#all logging
-    #for line in tailer.follow(open("/etc/gnb.log")):
-    	#print(line)
+    	gnb_logs = threading.Thread(target=self.get_gnb_logs).start()
     	#iperf
-    return 0
+    	
+    def get_gnb_logs(self):
+    	for line in tailer.follow(open('/tmp/gnb.log')):
+    	    print(line) #replace with send_log
+
+    def spinner_loading(self, handle, verbose=True):
+    	spinner = itertools.cycle(['|', '/', '-', '\\'])
+    	while not handle.initialized:
+            if verbose:
+            	sys.stdout.write(handle.output + '\n\n' + 50 * '=' + '\n')
+            sys.stdout.write(f"{handle.name} " + next(spinner))
+            sys.stdout.flush()
+            sys.stdout.write('\b')
+            time.sleep(0.1)
+            
+    def start_gnb(self):
+    	gnb_handle = Gnb()
+    	gnb_handle.start([sys.argv[0]])
+    	self.spinner_loading(gnb_handle, verbose=False)
+
+    
+    def start_core(self):
+    	core_handle = CoreNetwork()
+    	core_handle.start()
+    	self.spinner_loading(core_handle)
 
 def main():
-    gnb_handle = Gnb()
-    core_handle = CoreNetwork()
-    core_handle.start()
-    spinner_loading(core_handle)
-    gnb_handle.start([sys.argv[1]])
-    spinner_loading(gnb_handle, verbose=False)
-
+    controller = ue_controller()
+    controller.start()
+    
 
 if __name__ == '__main__':
     main()
