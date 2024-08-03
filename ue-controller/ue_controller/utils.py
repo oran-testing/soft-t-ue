779fe4d7dc0de317f1c43d8711123968bd52bd7e
Charles Ueltschey <chaseuelt@gmail.com>
2024-08-03 08:39:48 -0600

    prepare for merge

--- ue-controller/ue_controller/utils.py
+++ ue-controller/ue_controller/utils.py
@@ -1,20 +1,6 @@
 import subprocess
 
 
-def start_subprocess(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    return process
-
-
-def kill_subprocess(process):
-    process.terminate()  # Graceful termination
-    try:
-        process.wait(timeout=5)
-    except subprocess.TimeoutExpired:
-        process.kill()  # Forceful termination
-    process.communicate()
-
-
 def bytes_to_binary(byte_obj):
     """Convert a bytes object to a binary string."""
     return "".join(f"{byte:08b}" for byte in byte_obj)
