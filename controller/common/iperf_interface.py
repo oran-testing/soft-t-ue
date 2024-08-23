022f0dc3cc89e1a22f1be666bd0d4123c275687c
Charles Ueltschey <chaseuelt@gmail.com>
2024-08-23 16:35:36 -0600

    new log collection function

--- controller/common/iperf_interface.py
+++ controller/common/iperf_interface.py
@@ -1,6 +1,8 @@
 from common.utils import start_subprocess, kill_subprocess
 import threading
 import time
+import select
+import sys
 
 class Iperf:
     def __init__(self):
@@ -12,9 +14,9 @@ class Iperf:
 
     def start(self, args, process_type="server"):
         if process_type == "server":
-            command = ["iperf3", "-s"] + args
+            command = ["iperf3", "-s"] + args + ['2>&1']
         elif process_type == "client":
-            command = ["sudo", "ip", "netns","exec", "ue1", "iperf3", "-c"] + args
+            command = ["sudo", "ip", "netns","exec", "ue1", "iperf3", "-c"] + args + ['2>&1']
         else:
             raise ValueError("Invalid Process Type")
             return
@@ -33,14 +35,27 @@ class Iperf:
         self.name = "Iperf -- Stopped"
 
     def collect_logs(self):
+        stdout_fd = self.process.stdout.fileno()
+        stderr_fd = self.process.stderr.fileno()
+        poll = select.poll()
+        poll.register(stdout_fd, select.POLLIN)
+        poll.register(stderr_fd, select.POLLIN)
+
         while self.isRunning:
-            if self.process:
-                line = self.process.stdout.readline()
-                if line:
-                    self.output += '\n' + line.decode().strip()
-            else:
-                self.output += "Process Terminated"
-                break
+            events = poll.poll()
+            for fd, event in events:
+                if fd == stdout_fd:
+                    line = self.process.stdout.readline().decode()
+                    if line:
+                        self.output += f"{line.strip()}\n"
+                    else:
+                        poll.unregister(stdout_fd)
+                if fd == stderr_fd:
+                    line = self.process.stderr.readline().decode()
+                    if line:
+                        self.output += f"[Error]: {line.strip()}\n"
+                    else:
+                        poll.unregister(stderr_fd)
 
     def __repr__(self):
         return f"Iperf Process Object, running: {self.isRunning}"
--- controller/ue/ue_interface.py
+++ controller/ue/ue_interface.py
@@ -2,6 +2,11 @@ import threading
 import time
 import os
 import sys
+import select
+
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, parent_dir)
 
 from common.utils import start_subprocess, kill_subprocess
 from common.iperf_interface import Iperf
@@ -17,6 +22,7 @@ class Ue:
     def start(self, args):
         command = ["sudo", "srsue"] + args
         self.process = start_subprocess(command)
+        time.sleep(10)
         os.system("sudo ip netns exec ue1 ip ro add default via 10.45.1.1 dev tun_srsue")
         self.iperf_client.start(['-c', '10.53.1.1', '-t', '3000', '-u', '-b', '10M'], process_type='client')
         self.isRunning = True
@@ -30,14 +36,27 @@ class Ue:
         self.isRunning = False
 
     def collect_logs(self):
+        stdout_fd = self.process.stdout.fileno()
+        stderr_fd = self.process.stderr.fileno()
+        poll = select.poll()
+        poll.register(stdout_fd, select.POLLIN)
+        poll.register(stderr_fd, select.POLLIN)
+
         while self.isRunning:
-            if self.process:
-                line = self.process.stdout.readline()
-                if line:
-                    self.output += '\n' + line.decode().strip()
-            else:
-                self.output += "Process Terminated"
-                break
+            events = poll.poll()
+            for fd, event in events:
+                if fd == stdout_fd:
+                    line = self.process.stdout.readline().decode()
+                    if line:
+                        self.output += f"[Stdout]: {line}\n"
+                    else:
+                        poll.unregister(stdout_fd)
+                if fd == stderr_fd:
+                    line = self.process.stderr.readline().decode()
+                    if line:
+                        self.output += f"[Error]: {line}\n"
+                    else:
+                        poll.unregister(stderr_fd)
 
     def __repr__(self):
         return f"srsRAN UE object, running: {self.isRunning}"
@@ -47,8 +66,9 @@ if __name__ == "__main__":
     handle.start(["/home/ntia/Downloads/ue_zmq.conf"])
     time.sleep(20)
     while True:
-        print(handle.output)
-        print("\n\nIperf:")
-        print(handle.iperf_client.output)
+        sys.stdout.write(handle.output)
+        sys.stdout.flush()
+        sys.stdout.write(handle.iperf_client.output)
+        sys.stdout.flush()
         time.sleep(0.5)
 
