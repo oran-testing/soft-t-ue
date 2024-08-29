5dd496d01c134d3a78402ea913a884a1f4931586
Charles Ueltschey <chaseuelt@gmail.com>
2024-08-29 13:52:27 -0600

    added CQI attach to the python GUI

--- controller/ue/ue_controller.py
+++ controller/ue/ue_controller.py
@@ -118,8 +118,7 @@ class ProcessesPage(Screen):
     def add_ue(self, instance):
         self.popup.dismiss()
         new_ue = Ue(self.ue_index)
-        #global attack_args
-        attack_args = ["--phy.cqi_max", "200", "--phy.cqi_fixed", "200"]
+        global attack_args
         if self.ue_type == "tester":
             print(attack_args)
             new_ue.start([self.config_file] + attack_args)
@@ -171,7 +170,7 @@ class AttacksPage(Screen):
         layout = BoxLayout(orientation='vertical')
         attack_option = Spinner(
             text='Attack Type',
-            values=('None', 'SDU Fuzzing'),
+            values=('None', 'SDU Fuzzing', 'CQI Manipulation'),
             size_hint=(None, None),
             size=(400, 44)
         )
@@ -206,6 +205,16 @@ class AttacksPage(Screen):
             bits_to_fuzz.bind(text=self.set_fuzzed_bits)
             self.attack_settings.add_widget(target_message)
             self.attack_settings.add_widget(bits_to_fuzz)
+        if text == "CQI Manipulation":
+            cqi_value = Spinner(
+                text='CQI Value',
+                values=[str(i * 100) for i in range(10)],
+                size_hint=(None, None),
+                size=(200, 44)
+            )
+            cqi_value.bind(text=self.set_cqi_value)
+            self.attack_settings.add_widget(cqi_value)
+
 
     def set_target_message(self, spinner, text):
         self.target_message = text
@@ -221,6 +230,13 @@ class AttacksPage(Screen):
         attack_args = ["--rrc.sdu_fuzzed_bits", str(self.num_fuzzed_bits)
                        , "--rrc.fuzz_target_message", self.target_message]
         self.title.text = f"--rrc.sdu_fuzzed_bits {self.num_fuzzed_bits} --rrc.fuzz_target_message {self.target_message}"
+
+    def set_cqi_value(self, spinner, text):
+        global attack_args
+        attack_args = ["--phy.cqi_max", text, "--phy.cqi_fixed", text]
+        self.title.text = f"--phy.cqi_max {text} --phy.cqi_fixed {text}"
+
+
 
 
 class ResultsPage(Screen):
