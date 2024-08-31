3f33baab393c5c87c86baece55939866f531f903
Charles Ueltschey <chaseuelt@gmail.com>
2024-08-31 09:22:40 -0600

    refactor flooding attack into function

--- srsue/hdr/stack/rrc_nr/rrc_nr.h
+++ srsue/hdr/stack/rrc_nr/rrc_nr.h
@@ -141,8 +141,9 @@ public:
 private:
 
   // soft T UE additions
-  srsran::unique_byte_buffer_t fuzz_ccch_msg(srsran::unique_byte_buffer_t pdu, const asn1::rrc_nr::ul_ccch_msg_s msg);
-  srsran::unique_byte_buffer_t fuzz_dcch_msg(srsran::unique_byte_buffer_t pdu, const asn1::rrc_nr::ul_dcch_msg_s msg);
+  srsran::unique_byte_buffer_t fuzz_ccch_msg(srsran::unique_byte_buffer_t pdu, const asn1::rrc_nr::ul_ccch_msg_s msg, std::string msg_name);
+  srsran::unique_byte_buffer_t fuzz_dcch_msg(srsran::unique_byte_buffer_t pdu, const asn1::rrc_nr::ul_dcch_msg_s msg, std::string msg_name);
+  srsran::unique_byte_buffer_t signal_flood_ccch(uint32_t lcid, srsran::unique_byte_buffer_t pdu, std::string msg_name);
 
 
 
