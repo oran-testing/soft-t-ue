d4a519cd01d2485e616933d631d41338c9aba971
all655 <all655@msstate.edu>
2024-11-10 16:23:34 -0600

    Update install-gnb.sh

--- scripts/install-gnb.sh
+++ scripts/install-gnb.sh
@@ -36,16 +36,16 @@ apt-get install -y cmake make gcc g++ pkg-config libfftw3-dev libmbedtls-dev lib
 apt-get install -y libzmq3-dev
 apt-get install -y net-tools libboost-all-dev libconfig++-dev iperf3 git libxcb-cursor0? libgles2-mesa-dev?
 
-cd /opt
+# #cd /opt
 
 # Clone and install srsRAN
-git clone https://github.com/oran-testing/srsRAN_Project.git
-cd srsRAN_Project
-git checkout ue-tester
-mkdir build
-cd build
-cmake ../ -DENABLE_EXPORT=ON -DENABLE_ZEROMQ=ON
-make -j $(nproc)
-make install
+# # git clone https://github.com/oran-testing/srsRAN_Project.git
+# # cd srsRAN_Project
+# # git checkout ue-tester
+# # mkdir build
+# # cd build
+# # cmake ../ -DENABLE_EXPORT=ON -DENABLE_ZEROMQ=ON
+# # make -j $(nproc)
+# # make install
 
 set -x
