ae14a96fb5c92bd5b090e73220c66e15e3610a37
Charles Ueltschey <chaseuelt@gmail.com>
2024-11-13 10:03:37 -0600

    update headless test script

--- scripts/headless-test.sh
+++ scripts/headless-test.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
 
+PS4='[TEST: headless] '
+set -x
+
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+	echo "This script must be run as root."
+	exit 1
+fi
+
 SCRIPT_PATH=$(realpath "$0")
 SCRIPT_DIR=$(dirname $SCRIPT_PATH)
+GNB_CONFIG=$SCRIPT_DIR/../configs/zmq/gnb_zmq.yaml
+UE_CONFIG=$SCRIPT_DIR/../configs/zmq/ue_zmq.conf
 
-SESSION_1_NAME="gnb"
-SESSION_1_COMMAND="gnb -c $SCRIPT_DIR/../configs/zmq/gnb_zmq.yaml"
-SESSION_1_LOG="/tmp/tester_gnb.log"
+CORE_SESSION_NAME="5gc"
+CORE_SESSION_COMMAND="docker compose -f /opt/srsRAN_Project/docker/docker-compose.yml up --build 5gc"
+CORE_SESSION_LOG="/tmp/headless_5gc.log"
 
-SESSION_2_NAME="ue"
-SESSION_2_COMMAND="srsue $SCRIPT_DIR/../configs/zmq/ue_zmq.conf $@"
-SESSION_2_LOG="/tmp/soft_t_ue.log"
+GNB_SESSION_NAME="gnb"
+GNB_SESSION_COMMAND="gnb -c $SCRIPT_DIR/../configs/zmq/gnb_zmq.yaml"
+GNB_SESSION_LOG="/tmp/headless_gnb.log"
+
+UE_SESSION_NAME="ue"
+UE_SESSION_COMMAND="srsue $SCRIPT_DIR/../configs/zmq/ue_zmq.conf $@"
+UE_SESSION_LOG="/tmp/headless_ue.log"
 
 kill_existing_screen() {
 	local session_name=$1
@@ -35,36 +50,35 @@ start_screen_session() {
 	return 0
 }
 
-cd /opt/srsRAN_Project/docker/ && docker compose up --build 5gc -d
+ip netns add ue1 >/dev/null 2>&1
 
-echo "waiting for open5gs..."
+start_screen_session "$CORE_SESSION_NAME" "$CORE_SESSION_COMMAND" "$CORE_SESSION_LOG"
+CORE_STATUS=$?
+
 sleep 30
 
-start_screen_session "$SESSION_1_NAME" "$SESSION_1_COMMAND" "$SESSION_1_LOG"
-SESSION_1_STATUS=$?
-
-ip netns add ue1
+start_screen_session "$GNB_SESSION_NAME" "$GNB_SESSION_COMMAND" "$GNB_SESSION_LOG"
+GNB_STATUS=$?
 
 sleep 1
 
-start_screen_session "$SESSION_2_NAME" "$SESSION_2_COMMAND" "$SESSION_2_LOG"
-SESSION_2_STATUS=$?
+start_screen_session "$UE_SESSION_NAME" "$UE_SESSION_COMMAND" "$UE_SESSION_LOG"
+UE_STATUS=$?
 
 # Check if both sessions were started successfully
-if [ $SESSION_1_STATUS -ne 0 ] || [ $SESSION_2_STATUS -ne 0 ]; then
+if [ $CORE_STATUS -ne 0 ] || [ $GNB_STATUS -ne 0 ] || [ $UE_STATUS -ne 0 ]; then
 	echo "Failed to start screens"
 	exit 1
 fi
 
-sleep 10
 screen -ls
 screen -S "ue" -X attach
 
-kill_existing_screen "$SESSION_1_NAME"
-kill_existing_screen "$SESSION_2_NAME"
+kill_existing_screen "$UE_SESSION_NAME"
+kill_existing_screen "$GNB_SESSION_NAME"
+kill_existing_screen "$CORE_SESSION_NAME"
 
-cat $SESSION_1_LOG
-cat $SESSION_2_LOG
+cat $UE_SESSION_LOG
 
 if cat $SESSION_2_LOG | grep 'PDU Session'; then
 	echo "T UE Connected successfully"
@@ -73,3 +87,5 @@ else
 	echo "Connection Failed"
 	exit 1
 fi
+
+set -x
